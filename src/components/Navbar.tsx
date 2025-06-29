d26816cc0e77b2b98785aa1d8ade020ed0203561
DiegoMendez01 <diegomendez01soft@gmail.com>
2025-06-29 09:22:57 -0500

    Toogle navbar en movil

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,24 @@
-import '../assets/styles/Navbar.css';
 import { NavLink } from 'react-router-dom';
+import { useState } from 'react';
+import '../assets/styles/Navbar.css';
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleNavbar = () => setIsOpen(!isOpen);
+
   return (
     <nav className="main-navbar">
-      <ul className="navbar-list">
+      <button className="navbar-toggle" onClick={toggleNavbar} aria-label="Menú">
+        ☰
+      </button>
+
+      <ul className={`navbar-list ${isOpen ? 'open' : ''}`}>
         <li>
-          <NavLink to="/" className="navbar-link">Inicio</NavLink>
+          <NavLink to="/" className="navbar-link" onClick={() => setIsOpen(false)}>Inicio</NavLink>
         </li>
         <li>
-          <NavLink to="/login" title='Iniciar Sesión' className="navbar-link">Iniciar Sesión</NavLink>
+          <NavLink to="/login" className="navbar-link" onClick={() => setIsOpen(false)}>Iniciar Sesión</NavLink>
         </li>
       </ul>
     </nav>
