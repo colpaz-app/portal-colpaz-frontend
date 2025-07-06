4cb51a53cad3972917d420535020d8f427a613a0
DiegoMendez01 <diegomendez01soft@gmail.com>
2025-07-06 16:02:32 -0500

    Quitar paginador de 1 item

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -50,7 +50,7 @@ const Admin = () => {
         totalPages,
         paginatedData,
         goToPage,
-    } = usePagination(cardData, 1);
+    } = usePagination(cardData, 8);
 
     return (
         <div className="admin-page">
